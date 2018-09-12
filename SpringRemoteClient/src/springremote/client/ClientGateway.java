28643a2afb4fcc268ef0b48b024087d8a05c31b9
Srijit Paul <paul.srijit@gmail.com>
2018-09-12 12:33:28 +0530

    Client is working

--- SpringRemoteClient/src/springremote/client/ClientGateway.java
+++ SpringRemoteClient/src/springremote/client/ClientGateway.java
@@ -1,27 +1,26 @@
 package springremote.client;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import springremote.client.contract.TestRemoteClient;
 import springremote.client.dto.TestRequestDto;
 import springremote.client.dto.TestResponseDto;
+import springremote.context.ApplicationContextProvider;
 
 public class ClientGateway {
 	public void callRemoteService() {
-		final ApplicationContext context = new ClassPathXmlApplicationContext("/WEB-INF/applicationContext.xml");
-		if(context!=null) {
-			System.out.println("got context....");
-		}
-		TestRemoteClient remoteServiceRef = (TestRemoteClient) context.getBean("springRemoteClient-TestRemoteClient");
-		if(remoteServiceRef!=null) {
-			System.out.println("got bean....");
-		}
-		/*TestRequestDto requestDto = new TestRequestDto();
-		requestDto.setRequestString("This-is-a-test-request");
-		TestResponseDto responseDto = remoteServiceRef.doWork(requestDto);
 		
-		System.out.println("Reponse::"+responseDto.getResponseString());*/
+		TestRemoteClient bean = ApplicationContextProvider.getApplicationContext()
+				.getBean("springRemoteClient-TestRemoteClient", TestRemoteClient.class);
+
+		if (bean != null) {
+			System.out.println("got bean...");
+			
+			TestRequestDto requestDto = new TestRequestDto();
+			requestDto.setRequestString("This-is-a-test-request");
+			TestResponseDto responseDto = bean.doWork(requestDto);
+			
+			System.out.println("Reponse::"+responseDto.getResponseString());
+		}
+		
 	}
 	
 }
