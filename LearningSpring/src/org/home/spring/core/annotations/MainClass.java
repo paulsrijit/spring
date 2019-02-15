bac60e81bd6afc9aa166935f80e9d8d0a83f3f83
Srijit <Srijit@Srijit-PC>
2019-02-15 08:33:48 +0530

    Spring annotations

---
+++ LearningSpring/src/org/home/spring/core/annotations/MainClass.java
@@ -0,0 +1,9 @@
+package org.home.spring.core.annotations;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ LearningSpring/src/org/home/spring/core/annotations/conf/SpringConf.java
@@ -0,0 +1,10 @@
+package org.home.spring.core.annotations.conf;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages="org.home.spring.core.annotations")
+public class SpringConf {
+
+}
