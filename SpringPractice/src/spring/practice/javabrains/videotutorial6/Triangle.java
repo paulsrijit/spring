bb8346ad357a3bab47454057c6a275711046dd96
Srijit Paul <srijit.paul@cerner.com>
2019-03-13 12:44:16 +0530

    JavaBrains video tutorial 6

--- SpringPractice/src/spring/practice/javabrains/videotutorial6/Triangle.java
+++ SpringPractice/src/spring/practice/javabrains/videotutorial6/Triangle.java
@@ -3,14 +3,16 @@ package spring.practice.javabrains.videotutorial6;
 public class Triangle {
 	
 	private String type;
+	private Short area;
 
-	public Triangle(String type) {
+	public Triangle(String type, Short area) {
 		super();
 		System.out.println("Triangle constructor called.");
 		this.type = type;
+		this.area = area;
 	}
 
 	public void draw() {
-		System.out.println(type + " triangle drawn.");
+		System.out.println(type + " type triangle drawn with area " + area + ".");
 	}
 }
