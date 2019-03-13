d730e794262e6caeb501e7be9149620568322118
Srijit Paul <srijit.paul@cerner.com>
2019-03-13 14:32:20 +0530

    JavaBrains video tutorial 7

---
+++ SpringPractice/src/spring/practice/javabrains/videotutorial7/DrawingApp.java
@@ -0,0 +1,14 @@
+package spring.practice.javabrains.videotutorial7;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class DrawingApp {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext-videotutorial7.xml");
+		Triangle triangle = (Triangle) applicationContext.getBean("triangle");
+		triangle.draw();
+	}
+
+}
