7b7176c63844a756ca5932e3e0cfc5e1a9a0bdb0
paulsrijit <42836992+paulsrijit@users.noreply.github.com>
2019-03-17 14:27:50 +0530

    Test vid 7

---
+++ SpringDemo/src/org/javabrains/videotutorials/tutorial4/Triangle.java
@@ -0,0 +1,7 @@
+package org.javabrains.videotutorials.tutorial4;
+
+public class Triangle {
+	public void draw() {
+		System.out.println("triangle drawn!");
+	}
+}
--- SpringPractice/src/spring/practice/javabrains/videotutorial7/DrawingApp.java
+++ SpringPractice/src/spring/practice/javabrains/videotutorial7/DrawingApp.java
@@ -3,7 +3,7 @@ package spring.practice.javabrains.videotutorial7;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class DrawingApp {
+public class DrawingApp {//Tutorial 7
 
 	public static void main(String[] args) {
 		ApplicationContext applicationContext = new ClassPathXmlApplicationContext("applicationContext-videotutorial7.xml");
